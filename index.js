ed1b74aa71ddbc77db6f26e10cad32f8dba0e0e7
Samuel Brooker - WL <samuelbrookerwl@Samuels-MBP-WL.gateway>
2023-10-10 13:35:25 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//export
+export const sum = (a, b) => {
+    return a + b;
+};
---
+++ index.test.js
@@ -0,0 +1,9 @@
+import { sum } from "./index";
+
+describe("Testing sum()", () => {
+    it("should return the right results with valid arguments", () => {
+        expect(sum(2, 2)).toBe(4);
+        expect(sum(1.5, 2.5)).toBe(4);
+        expect(sum(-2, -2)).toBe(-4);
+    });
+});
